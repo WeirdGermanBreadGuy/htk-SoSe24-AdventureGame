7b5b3fbfc04642e3c2822c45c84ea1064a55eac3
TheRealWeeb <58658677+TheRealWeeb@users.noreply.github.com>
2024-04-26 17:53:53 +0200

    added DialogueChoiceAnimations

--- Assets/Dialogue/Mother.ink
+++ Assets/Dialogue/Mother.ink
@@ -1,12 +1,14 @@
-Mama: Du kleines- Aargh, Raha! Wenn ich dich in die Finger kriege!
+#speaker Mama
+=== Intro ===
 #anim Matthew stands up and leaves
+Du kleines- Aargh, Raha! Wenn ich dich in die Finger kriege!
 * Was ist denn hier passiert?
-    Mama: Drei Mal darfst du raten. Raha hat schon wieder alles hier verwüstet. Wahrscheinlich wollte sie wieder was zu Essen ergaunern. Sie macht mich noch fertig!
-    -> Chaos
+    Drei Mal darfst du raten. Raha hat schon wieder alles hier verwüstet. Wahrscheinlich wollte sie wieder was zu Essen ergaunern. Sie macht mich noch fertig!
+        -> Chaos
 * Wer hat denn den Krug umgestoßen?
-    Mama: Erinnerst du dich noch an dieses Vieh mit den großen Ohren, was sich Essen stibitzten wollte? Das war wieder hier.
--> Chaos
+    Erinnerst du dich noch an dieses Vieh mit den großen Ohren, was sich Essen stibitzten wollte? Das war wieder hier.
+      -> Chaos
 
 == Chaos==
-Mama: Ich werde das Chaos hier am besten aufräumen.
+Ich werde das Chaos hier am besten aufräumen.
     -> DONE
--- Assets/Scripts/StoryView.cs
+++ Assets/Scripts/StoryView.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using Ink.Runtime;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.iOS;
 using UnityEngine.UI;
 
 public class StoryView : MonoBehaviour
@@ -15,6 +18,17 @@ public class StoryView : MonoBehaviour
     [SerializeField] private TextMeshProUGUI storyText;
     [SerializeField] private TextMeshProUGUI speakerName;
     [SerializeField] private Button buttonPrefab;
+
+    [SerializeField] private Image speakerImage;
+
+    [SerializeField] private List<SpeakerConfig> speakerConfigs;
+
+    [Serializable]
+    public class SpeakerConfig
+    {
+        public string name;
+        public Sprite sprite;
+    }
 
     private void Awake()
     {
@@ -33,7 +47,7 @@ public class StoryView : MonoBehaviour
 
         ShowStory();
     }
-    
+
     private void CloseStory()
     {
         Cursor.visible = false;
@@ -63,14 +77,14 @@ public class StoryView : MonoBehaviour
             for (int i = 0; i < story.currentChoices.Count; i++)
             {
                 Choice choice = story.currentChoices[i];
-                Button button = CreateChoiceView(choice.text.Trim());
+                Button button = CreateChoiceView(choice.text.Trim(), i);
                 // Tell the button what to do when we press it
-                button.onClick.AddListener( () => OnClickChoiceButton(choice));
+                button.onClick.AddListener(() => OnClickChoiceButton(choice));
             }
         }
         else
         {
-            Button choice = CreateChoiceView("Continue");
+            Button choice = CreateChoiceView("Continue", 0);
             choice.onClick.AddListener(CloseStory);
         }
     }
@@ -83,20 +97,35 @@ public class StoryView : MonoBehaviour
 
     private void CreateContentView(string text)
     {
-        string[] parts = text.Split(':');
-        if (parts.Length >= 2)
-        {
-            speakerName.text = parts[0];
-            storyText.text = parts[1];
-        }
-        else
-        {
-            speakerName.text = string.Empty;
-            storyText.text = text;
-        }
+        var speaker = story.globalTags.FirstOrDefault(t => t.Contains("speaker"))?.Split(' ')[1];
+        speakerName.text = speaker;
+        speakerImage.sprite = GetSpeakerImage(speaker);
+        StartCoroutine(ShowTextLetterByLetter(text));
+    }
+    
+    
+    private Sprite GetSpeakerImage(string speaker)
+    {
+        return speakerConfigs.FirstOrDefault(s => s.name == speaker)?.sprite;
     }
 
-    private void DestroyOldChoices()
+    private IEnumerator ShowTextLetterByLetter(string text)
+    {
+        storyText.text = text;
+        storyText.maxVisibleCharacters = 0;
+        for (int i = 0; i <= text.Length; i++)
+        {
+            storyText.maxVisibleCharacters = i;
+            if (Keyboard.current.spaceKey.wasPressedThisFrame)
+            {
+                storyText.maxVisibleCharacters = text.Length;
+            }
+        }
+
+        yield return new WaitForSeconds(0.025f);
+    }
+
+private void DestroyOldChoices()
     {
         foreach (Transform child in choiceHolder)
         {
@@ -104,9 +133,13 @@ public class StoryView : MonoBehaviour
         }
     }
     
-    private Button CreateChoiceView(string text)
+    private Button CreateChoiceView(string text, int index)
     {
         var choice = Instantiate(buttonPrefab, choiceHolder.transform, false);
+        if (index == 0)
+        {
+            choice.Select();
+        }
 
         var choiceText = choice.GetComponentInChildren<TextMeshProUGUI>();
         choiceText.text = text;
